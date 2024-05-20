d677a309e469618492c3fc3bc1e968c79858fd73
daiveekj <daiveekjawalkar123@gmail.com>
2024-05-20 23:20:28 +0530

    qrcode

--- src/qrcodegenerator/QRCodeGenerator .jsx
+++ src/qrcodegenerator/QRCodeGenerator .jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import QRCode from 'qrcode.react';
-import "./qrcodegenerater.css"
+import "./qrcodegenerater.css" // Make sure to adjust the file path if needed
 
 const QRCodeGenerator = () => {
   const [url, setUrl] = useState('');
@@ -9,32 +9,29 @@ const QRCodeGenerator = () => {
     setUrl(event.target.value);
   };
 
-  return (
-    // <div className="content-box">
-    //   <div id='main'>
-    //   <label htmlFor="urlInput">Enter URL:</label>
-    //   <input
-    //     type="text"
-    //     id="urlInput"
-    //     value={url}
-    //     onChange={handleUrlChange}
-    //     placeholder="Enter URL here"
-    //   />
+  const handleDownload = () => {
+    const canvas = document.getElementById('qrcode').getElementsByTagName('canvas')[0];
 
-    //   {url && (
-    //     <div id='qrcode'>
-    //       <QRCode value={url} />
-    //     </div>
-    //   )}
-    // </div>
-    // </div>
+    if (canvas) {
+      const url = canvas.toDataURL('image/png');
+      const link = document.createElement('a');
+      link.href = url;
+      link.download = 'qrcode.png';
+      link.click();
+    }
+  };
+
+  return (
     <div className="main">
       <div id="content">
-        <label htmlFor="">Enter the URL :</label>
-        <input type="text" placeholder='Enter the URL' value={url} onChange={handleUrlChange}/>
+        <label htmlFor="">Enter the URL:</label>
+        <input type="text" placeholder="Enter the URL" value={url} onChange={handleUrlChange} />
       </div>
       <div id="qrcode">
-        <QRCode value={url}/>
+        <QRCode value={url} />
+      </div>
+      <div className="button">
+        <button onClick={handleDownload}>Click to Download</button>
       </div>
     </div>
   );
